2c1187937d81689ebd449e951ed1668c0f03eed5
roy301994 <tristantofir@gmail.com>
2021-09-20 03:35:58 +0700

    perubahan

--- cypress/integration/lihat.js
+++ cypress/integration/lihat.js
@@ -1,10 +1,23 @@
-describe('adminDojobox', function () {
-    it('Lihat Facilitator', function () {
-        cy.login()
-        cy.get(':nth-child(1) > .nav-link > span').click()
-        cy.get(':nth-child(1) > .dt-center > .btn-group > .btn > .fas').click()
-        cy.get('[href="facilitator/detail/1"]').click()
-        cy.wait(5000)
-        cy.get('.justify-content-end > .btn-primary').click()
+describe('Task 5',()=>{
+    it('Task 5',()=>{
+        cy.openurl()
+    })
+
+    it('Isi form',()=>{
+        cy.get('#firstName').type('roy').should('have.value','roy')
+        cy.get('#lastName').type('arto').should('have.value','arto')
+        cy.get('#userEmail').type('arto@sasongko.com').should('have.value','arto@sasongko.com')
+        cy.get('#genterWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click()
+        cy.get('#userNumber').type('12345').should('have.value','12345')
+        cy.get('#dateOfBirthInput').should('be.visible').type('21 Aug 2000',{force:true})
+        cy.get('#dateOfBirthInput').type('21 Aug 2000')
+        cy.get('datepicker__year-select').contains('2000').click()
+
+        cy.get('#dateOfBirthInput').type('21 Jul 2000')
+        .should('have.value','21 jul 2000')
+        cy.get('.subjects-auto-complete__value-container').type('mahasiswa').should('have.value','mahasiswa')
+        cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(3) > .custom-control-label').click()
+       
+
     })
 })
--- cypress/integration/login.js
+++
@@ -1,9 +0,0 @@
-describe('adminDojobox', function () {
-    it('login testing 1', function () {
-        cy.visit('https://admin.pkh.dojobox.id/')
-        cy.get('#input-email').type('e@dojobox.id')
-        cy.get('#input-password').type('admin')
-        cy.wait(10000)
-        cy.get('#btn-login').click()
-    })
-})
